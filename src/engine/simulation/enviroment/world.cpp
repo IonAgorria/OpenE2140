d409399a3e2e03d2e05e58cc99c3309b2a35e798
Ion Agorria <ion@agorria.com>
2019-05-08 22:45:14 +0200

    Add basic world code

--- src/engine/simulation/enviroment/world.cpp
+++ src/engine/simulation/enviroment/world.cpp
@@ -1,12 +1,25 @@
 //
 // Created by Ion Agorria on 20/05/18
 //
+#include "engine/graphics/renderer.h"
 #include "world.h"
 
 World::World(const asset_path& path) {
 }
 
-bool World::draw(Window& window, Rectangle& rectangle) {
+void World::update() {
+    size_t size = tiles.size();
+    for (size_t i = 0; i < size; ++i) {
+        Tile& tile = *tiles[i];
+
+        //Update image for tile
+        if (tile.isImageDirty) {
+            tileImages[i] = calculateTileImage(tile);
+        }
+    }
+}
+
+void World::draw(Renderer& renderer, const Rectangle& rectangle) {
     //Do pixel to tile conversions
     int drawStartX = rectangle.x % tileSize;
     int drawStartY = rectangle.y % tileSize;
@@ -18,21 +31,53 @@ bool World::draw(Window& window, Rectangle& rectangle) {
     int tileCountY = tileEndY - tileStartY;
 
     //Create rectangle for drawing operations
-    Rectangle destination = Rectangle(0, 0, tileSize * scaling, tileSize * scaling);
-
+    int drawTileSize = tileSize * scaling;
     //Iterate each tile inside rectangle
     for (int y = tileStartY; y < tileEndY; y++) {
         for (int x = tileStartX; x < tileEndX; x++) {
-            //Get current image of tile
-            std::shared_ptr<Image> image = tileImages[x + worldRectangle.w * y];
-
-            //Set the destination position
-            destination.x = drawStartX + ((x - tileCountX) * destination.w);
-            destination.y = drawStartY + ((y - tileCountY) * destination.h);
-            if (!window.draw(*image, destination)) {
-                return false;
+            //Get current image of tile and draw it
+            std::shared_ptr<Image> image = tileImages.at(x + worldRectangle.w * y);
+            if (!image) {
+                continue;
             }
+            renderer.draw(
+                drawStartX + ((x - tileCountX) * drawTileSize),
+                drawStartY + ((y - tileCountY) * drawTileSize),
+                drawTileSize,
+                drawTileSize,
+                0,
+                *image,
+                nullptr
+            );
         }
     }
-    return true;
+}
+
+
+Tile* World::getTile(unsigned long index) {
+    if (index < 0 || index >= tiles.size()) {
+        return nullptr;
+    }
+    return tiles.at(index).get();
+}
+
+Tile* World::getTile(unsigned long x, unsigned long y) {
+    return getTile(x + worldRectangle.w * y);
+}
+
+Tile* World::getTile(const Vector2& position) {
+    return getTile(position.x / tileSize, position.y / tileSize);
+}
+
+void World::toTilePosition(const Vector2& position, Vector2& result) {
+    result.set(position.x / tileSize, position.y / tileSize);
+}
+
+void World::toWorldPosition(const Vector2& position, Vector2& result) {
+    result.set(position.x * (tileSize + tileSizeHalf), position.y * (tileSize + tileSizeHalf));
+}
+
+std::shared_ptr<Image> World::calculateTileImage(Tile& tile) {
+    tile.isImageDirty = false;
+    return std::shared_ptr<Image>();
 }
--- src/engine/simulation/enviroment/world.h
+++ src/engine/simulation/enviroment/world.h
@@ -6,10 +6,15 @@
 
 #include <vector>
 #include "core/common.h"
-#include "graphics/window.h"
+#include "math/rectangle.h"
 #include "assets/asset.h"
 #include "tile.h"
 
+class Renderer;
+class Image;
+/**
+ * Contains the world data such as tiles
+ */
 class World {
 private:
     /**
@@ -20,17 +25,22 @@ private:
     /**ŧ
      * Images to be drawn
      */
-    const std::vector<std::shared_ptr<Image>> tileImages;
+    std::vector<std::shared_ptr<Image>> tileImages;
 
     /**
      * Tiles information
      */
-    const std::vector<Tile> tiles;
+    std::vector<std::unique_ptr<Tile>> tiles;
 
     /**
      * Tile image size
      */
     const int tileSize = 64;
+
+    /**
+     * Tile image size half
+     */
+    const int tileSizeHalf = tileSize / 2;
 
     /**
      * World scaling
@@ -54,13 +64,68 @@ public:
     NON_COPYABLE_NOR_MOVABLE(World)
 
     /**
-     * Draws the world using provided view
+     * Updates the world state
      *
-     * @param window to use for drawing
-     * @param view window
      * @return true if success
      */
-     bool draw(Window& window, Rectangle& rectangle);
+    void update();
+
+    /**
+     * Draws the world using provided view
+     *
+     * @param renderer to use for drawing
+     * @param view window
+     */
+    void draw(Renderer& renderer, const Rectangle& rectangle);
+
+    /**
+     * Tile in specified tile index
+     *
+     * @param index of tile in world
+     * @return tile if valid
+     */
+    Tile* getTile(unsigned long index);
+
+    /**
+     * Tile in specified tile position
+     *
+     * @param index of tile in world
+     * @return tile if valid
+     */
+    Tile* getTile(unsigned long x, unsigned long y);
+
+    /**
+     * Tile in specified world position
+     *
+     * @param index of tile in world
+     * @return tile if valid
+     */
+    Tile* getTile(const Vector2& position);
+
+    /**
+     * Translates world position to tile position
+     *
+     * @param position to convert
+     * @param result to write result
+     * @return tile if valid
+     */
+    void toTilePosition(const Vector2& position, Vector2& result);
+
+    /**
+     * Translates tile position to world position
+     *
+     * @param position to convert
+     * @param result to write result
+     * @return tile if valid
+     */
+    void toWorldPosition(const Vector2& position, Vector2& result);
+
+    /**
+     * Calculates the image for the tile
+     * @param tile
+     * @return
+     */
+    std::shared_ptr<Image> calculateTileImage(Tile& tile);
 };
 
 

eb3d9dc7ae6d0d00f1cf6aca9f0fdccb55118737
Ion Agorria <ion@agorria.com>
2019-06-17 23:29:04 +0200

    Component wip

---
+++ src/game/components/player_component.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ion Agorria on 13/06/19
+//
+#include "player_component.h"
+
+CLASS_COMPONENT_DEFAULT(PlayerComponent);
+
+void PlayerComponent::simulationChanged() {
+}
+
+void PlayerComponent::update() {
+}
